94c3e316b7863a8e5876bf25f1bb2c60a84b58c9
Emma Halsey <emma.halsey@ehi.com>
2023-07-25 08:30:03 -0500

    add final images

--- src/SummerProject.jsx
+++ src/SummerProject.jsx
@@ -1,52 +1,13 @@
 import React from "react";
+import Slate from './takoImages/slate.jpg';
+import Work from './takoImages/workaholic.jpg';
+import AWS from './takoImages/aws.jpg';
+
 export default function SummerProject() {
   return (
     <div className="container">
       <div className="row">
         <div className="col-lg-2"></div>
-        {/* <div className="col-lg-8">
-          <div className="accordion" id="accordionPanelsStayOpenExample">
-            <div className="accordion-item">
-              <h2 className="accordion-header" id="panelsStayOpen-headingOne">
-                <button className="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseOne" aria-expanded="true" aria-controls="panelsStayOpen-collapseOne">
-                  Challenges & Solutions
-                </button>
-              </h2>
-              <div id="panelsStayOpen-collapseOne" className="accordion-collapse collapse show" aria-labelledby="panelsStayOpen-headingOne">
-                <div className="accordion-body">
-
-                  <h6>Making things too complicated</h6>
-                  <p>read documentation</p>
-                  <p>trust libraries do things</p>
-
-                </div>
-              </div>
-            </div>
-            <div className="accordion-item">
-              <h2 className="accordion-header" id="panelsStayOpen-headingTwo">
-                <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseTwo" aria-expanded="false" aria-controls="panelsStayOpen-collapseTwo">
-                  Have I worked on anything else??
-                </button>
-              </h2>
-              <div id="panelsStayOpen-collapseTwo" className="accordion-collapse collapse" aria-labelledby="panelsStayOpen-headingTwo">
-                <div className="accordion-body">
-                  <strong>Yes.</strong> AWS Cloud Practitioner training and a bit in Tako-ui back end.
-                </div>
-              </div>
-            </div>
-            <div className="accordion-item">
-              <h2 className="accordion-header" id="panelsStayOpen-headingThree">
-                <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseThree" aria-expanded="false" aria-controls="panelsStayOpen-collapseThree">
-                  Demo!
-                </button>
-              </h2>
-              <div id="panelsStayOpen-collapseThree" className="accordion-collapse collapse" aria-labelledby="panelsStayOpen-headingThree">
-                <div className="accordion-body">
-                  <strong>This is the third item's accordion body.</strong> <a href="http://localhost:5173/?clientId=39df573a-f2f7-4eae-9f6e-3bac796aae02&preAuthCode=fakeCode#">Tako-Ui-Fe</a>
-                </div>
-              </div>
-            </div>
-          </div></div> */}
         <div className="col-lg-8">
           <div className="accordion" id="accordionExample">
             <div className="accordion-item">
@@ -59,6 +20,7 @@ export default function SummerProject() {
                 <div className="accordion-body">
                   <p>I got to experience the whole development process from an empty repository to a working app</p>
                   <p>Tako-Ui-Fe is kind of isolated. It's time to learn new technologies</p>
+                  <img src={Slate} alt="" />
                 </div>
               </div>
             </div>
@@ -72,6 +34,7 @@ export default function SummerProject() {
                 <div className="accordion-body">
                   <p>It's worth reading the documentation</p>
                   <p>Don't do more work than you have to.</p>
+                  <img src={Work} alt="" />
                 </div>
               </div>
             </div>
@@ -85,6 +48,7 @@ export default function SummerProject() {
                 <div className="accordion-body">
                   <p>Connect with others around the organization</p>
                   <p>Start working toward AWS Cloud Practitioner Certification</p>
+                  <img src={AWS} alt="" />
                 </div>
               </div>
             </div>
